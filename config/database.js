63b8b3c96aaee81b02dbf325fcf16ef8961458cb
Muhammad-Massab <muhammadmassabhassan@gmail.com>
2024-10-21 22:33:25 +0500

    Fix pg error

--- config/database.js
+++ config/database.js
@@ -8,6 +8,7 @@ const sequelize = new Sequelize(
   {
     host: process.env.POSTGRES_HOST,
     dialect: "postgres",
+    dialectModule: pg,
     dialectOptions: {
       ssl: {
         require: true,
